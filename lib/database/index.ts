ff1f63699887e61fbefc713bfea13e733cd7ac18
Chavi Awasthy <93931120+chaviawasthy@users.noreply.github.com>
2024-02-02 22:27:53 +0530

    updates2

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-const MONGODB_URI = process.env.MONGOOB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
